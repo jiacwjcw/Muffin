98cb030faf10d7b55f877a00c1d7c4842f365799
jc <jc.wu@gogolook.com>
2024-01-30 14:35:46 +0800

    feat: add testrail_client via testrail api

---
+++ tests/test_testrail_client.py
@@ -0,0 +1,69 @@
+import unittest
+
+from modules.testrail_client import TestrailClient
+
+
+class TestTestrailClient(unittest.TestCase):
+    def test_init_error_handle(self):
+        cases = [
+            {
+                "name": "no host",
+                "test_data": {
+                    "host": None,
+                    "username": "abc",
+                    "password": "abc",
+                    "project_id": "123",
+                    "suite_id": "123",
+                },
+            },
+            {
+                "name": "no username",
+                "test_data": {
+                    "host": "http://abc.com",
+                    "username": None,
+                    "password": "abc",
+                    "project_id": "123",
+                    "suite_id": "123",
+                },
+            },
+            {
+                "name": "no password",
+                "test_data": {
+                    "host": "http://abc.com",
+                    "username": "abc",
+                    "password": None,
+                    "project_id": "123",
+                    "suite_id": "123",
+                },
+            },
+            {
+                "name": "no project_id",
+                "test_data": {
+                    "host": "http://abc.com",
+                    "username": "abc",
+                    "password": "abc",
+                    "project_id": None,
+                    "suite_id": "123",
+                },
+            },
+            {
+                "name": "no suite_id",
+                "test_data": {
+                    "host": "http://abc.com",
+                    "username": "abc",
+                    "password": "abc",
+                    "project_id": "123",
+                    "suite_id": None,
+                },
+            },
+        ]
+        for case in cases:
+            test_data = case["test_data"]
+            with self.assertRaises(ValueError, msg=f"Case: {case['name']} - failed"):
+                TestrailClient(
+                    host="123",
+                    username=test_data["username"],
+                    password=test_data["password"],
+                    project_id=test_data["project_id"],
+                    suite_id=test_data["suite_id"],
+                ),
